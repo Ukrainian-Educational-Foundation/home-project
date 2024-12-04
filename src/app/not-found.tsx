b99725cca1edb780bf75d9f4c540363813f111fc
Volodymyr <davidvovchik@gmail.com>
2024-12-04 17:37:49 +0300

    new project

---
+++ src/app/not-found.tsx
@@ -0,0 +1,5 @@
+import styles from "./page.module.css";
+
+export default function NotFound() {
+  return <div className={styles.page}>Page Not Found!</div>;
+}
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,35 @@
+import Button from "@/ui-kit/Button/Button";
+import styles from "./Header.module.css";
+
+import React from "react";
+import Image from "next/image";
+
+function Header() {
+  return (
+    <div className={`${styles.header}`}>
+      <div>
+        <Image src="/Fund_logo.png" alt="logo" width={180} height={60} />
+      </div>
+      <div className={`${styles.buttons}`}>
+        <button>Головна</button>
+        <button>Про проект</button>
+        <button>Про фонд</button>
+        <button>Контакти</button>
+      </div>
+      <div>
+        <Button text="ХОЧУ ДОПОМОГТИ" size="Small" />
+        <ul>
+          <li>
+            <button>UA</button>
+          </li>
+          <li className="line"></li>
+          <li>
+            <button>EN</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
