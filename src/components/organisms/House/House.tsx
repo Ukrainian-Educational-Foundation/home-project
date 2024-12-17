01ca38bbb4dc0fe33b3ce637e968f06481248009
Volodymyr <davidvovchik@gmail.com>
2024-12-17 17:40:37 +0300

    mobile

--- src/components/organisms/House/House.tsx
+++ src/components/organisms/House/House.tsx
@@ -6,6 +6,7 @@ import React, { useEffect, useState } from "react";
 
 function House() {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isTablet, setIsTablet] = useState(false);
 
   const images = [
     ["/Ellipse1.png.webp", "/Ellipse2.png.webp", "/Ellipse3.png.webp"],
@@ -15,6 +16,7 @@ function House() {
     ["/cycle2.webp", "/Ellipse1.png.webp", "/Ellipse2.png.webp"],
   ];
 
+  // Следим за сменой индекса
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
@@ -23,23 +25,42 @@ function House() {
     return () => clearInterval(interval);
   }, [images.length]);
 
+  // Следим за изменением размеров экрана
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 1024px)");
+
+    const handleResize = (event: MediaQueryListEvent | MediaQueryList) => {
+      setIsTablet(event.matches);
+    };
+
+    // Устанавливаем начальное значение
+    handleResize(mediaQuery);
+
+    // Добавляем слушателя
+    mediaQuery.addEventListener("change", handleResize);
+
+    // Очищаем слушателя при размонтировании компонента
+    return () => mediaQuery.removeEventListener("change", handleResize);
+  }, []);
+
   return (
     <div className={styles.house}>
       <div>Що буде з проектом далі</div>
       <ul>
         <li>
           Для родин полеглих героїв, тих, хто
-          <br /> перебуває в полоні або отримав
-          <br /> поранення, а також для дітей, чиї
-          <br /> батьки працюють у ДСНС чи є<br />
+          {isTablet ? null : <br />} перебуває в полоні або отримав
+          {isTablet ? null : <br />} поранення, а також для дітей, чиї
+          {isTablet ? null : <br />} батьки працюють у ДСНС чи є
+          {isTablet ? null : <br />}
           медичними працівниками, у Центрі
-          <br />
+          {isTablet ? null : <br />}
           &quot;ДІМ&quot; діятиме програма
-          <br /> реабілітації. У центрі
-          <br /> працюватимуть кваліфіковані
-          <br /> психологи, а також будуть
-          <br /> організовані групові заняття для
-          <br /> підтримки та відновлення.
+          {isTablet ? null : <br />} реабілітації. У центрі
+          {isTablet ? null : <br />} працюватимуть кваліфіковані
+          {isTablet ? null : <br />} психологи, а також будуть
+          {isTablet ? null : <br />} організовані групові заняття для
+          {isTablet ? null : <br />} підтримки та відновлення.
         </li>
         <li>
           <Image src="/Home.png.webp" alt="logo" fill sizes="auto" />
