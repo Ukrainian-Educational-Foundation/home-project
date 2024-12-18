e0d1bc323f5a7de32a3ab8a0783039951828f243
Inna Didenko <innadidenko29@gmail.com>
2024-12-18 19:07:57 +0200

    add toggle language

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
       <Slider />
       <HelpDescribe />
       <House />
-      <AboutFond/>
+      <AboutFond />
       <TrustUs />
       <ContactUs />
     </div>
